94104d497250a4b820dc2910d46aa31a026982cf
Hyunmin Kang <hmkang@hmkang.com>
2012-05-23 15:46:54 +0900

    Initial Commit

---
+++ CH10_iOSBackgroundingTone/CH10_iOSBackgroundingToneAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CH10_iOSBackgroundingToneAppDelegate.h
+//  CH10_iOSBackgroundingTone
+//
+//  Created by hmkang on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CH10_iOSBackgroundingToneAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CH10_iOSBackgroundingTone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CH10_iOSBackgroundingTone
+//
+//  Created by hmkang on 5/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CH10_iOSBackgroundingToneAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CH10_iOSBackgroundingToneAppDelegate class]));
+    }
+}
